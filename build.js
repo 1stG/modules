4388ce21eab578ca2db923c74b7bb6580bfe462d
JounQin <jiaren.ljr@alibaba-inc.com>
2018-12-02 19:14:35 +0800

    feat: add unpkg prefix

--- build.js
+++ build.js
@@ -17,21 +17,33 @@ const MODULES_MAP = {
   moment: isDev ? 'moment' : 'min/moment.min',
 }
 
+const MODULES_MAP_ENTRIES = Object.entries(MODULES_MAP)
+
 const DIST_PATH = resolve('dist')
+
+const UNPKG_PREFIX = '//unpkg.com/'
 
 mkdirSync(DIST_PATH, { recursive: true })
 
 writeFileSync(
   resolve(DIST_PATH, 'requirejs-paths.json'),
-  JSON.stringify(MODULES_MAP, null, 2),
+  JSON.stringify(
+    MODULES_MAP_ENTRIES.reduce(
+      (packages, [key, value]) =>
+        Object.assign(packages, { [key]: `${UNPKG_PREFIX}${key}/${value}` }),
+      {},
+    ),
+    null,
+    2,
+  ),
 )
 
 writeFileSync(
   resolve(DIST_PATH, 'systemjs-packages.json'),
   JSON.stringify(
-    Object.entries(MODULES_MAP).reduce(
+    MODULES_MAP_ENTRIES.reduce(
       (packages, [key, value]) =>
-        Object.assign(packages, { [key]: value + '.js' }),
+        Object.assign(packages, { [key]: `${UNPKG_PREFIX}${key}/${value}.js` }),
       {},
     ),
     null,
